97e2c50b63e11b248892e81341ebc2652005512a
lkusm <otake.miki@ibm.com>
2026-02-17 06:53:19 +0000

    Change placeholderValue

--- src/frontend/src/components/agents/chatbot/ChatInput.tsx
+++ src/frontend/src/components/agents/chatbot/ChatInput.tsx
@@ -44,7 +44,7 @@ export const ChatInput: React.FC<ChatInputProps> = ({
       onSubmit={() => {
         onMessageSend(inputText ?? "");
       }}
-      placeholderValue="Type your message here..."
+      placeholderValue="Feel free to ask anything here..."
     >
       <ImperativeControlPlugin ref={controlRef} />
     </ChatInputFluent>
